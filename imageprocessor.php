c76caa2a8fb2b1a8cf5bbbc30ea668c3e41bd664
soum <soum@soums-MBP.hsd1.pa.comcast.net>
2016-06-21 10:24:51 -0400

    dynamic file name

--- imageprocessor.php
+++ imageprocessor.php
@@ -1,29 +1,26 @@
 <?php
 
-	function get_new_img_url($imgURL, $text){
+	function get_new_img_url($imgURL, $text, $order){
 		
-		return LoadJPEG($imgURL, $text);		
+		return LoadJPEG($imgURL, $text, $order);		
 
 	}
 
-	function LoadJPEG ($url, $text) {
-
+	function LoadJPEG ($url, $text, $order) {
 
 	    $jpg_image = imagecreatefromjpeg($url);
 
 	    //height & width
 	    $width = imagesx($jpg_image);
   		$height = imagesy($jpg_image);
-  		//to do: use this to center the text over image
 
+  		if(!empty($width) && !empty($height)){
 
-  		//allocate font and color
-  		if(!empty($width)){
 			$white = imagecolorallocate($jpg_image, 255, 255, 255);
 			$font_path = 'OpenSans-Bold.TTF';
 			
 			imagettftext($jpg_image, 25, 0, 75, 150, $white, $font_path, $text);
-			imagejpeg($jpg_image,"temp/image.jpg");
+			imagejpeg($jpg_image,"temp/".$order.".jpg");
 			imagedestroy($jpg_image);
 
 			return true;
@@ -32,15 +29,6 @@
 		}
 
 	}
-
-
-	function write_to_sftp(){
-		//to do::
-		// write a batch job
-		//loop over local files and post them over sftp
-		//move files to archieve directory with new time stamp sub-directory 
-	}
-
 
 
 ?>
