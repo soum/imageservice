3597f0d5b0b9fa673fc9c95fc81fbde43ae1dc0f
soum <soum@soums-MBP.hsd1.pa.comcast.net>
2016-06-28 19:50:53 -0400

    added service key validation

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	header("Content-Type:application/json");
-	include("ServiceKeyVerification.php");
+	include("ServiceKeyValidation.php");
 	include("imageprocessor.php");
 
 	$img_url = $_GET['url'];
